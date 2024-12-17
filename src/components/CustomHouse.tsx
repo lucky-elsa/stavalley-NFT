0c081ea07e630b70751e92daf93a12c498252d71
lucky-elsa <beautylotus718@gmail.com>
2024-12-17 17:27:13 +0800

    fix: updated play and join community component

--- src/components/CustomHouse.tsx
+++ src/components/CustomHouse.tsx
@@ -1,7 +1,7 @@
 const CustomHouse = () => {
   return (
-    <div className="flex justify-center items-center w-full h-[879px] bg-[url('/img/background-2.png')] bg-cover bg-center bg-no-repeat">
-      <p className="quantico">Custom House</p>
+    <div className="flex justify-center items-center w-full h-[730px] bg-[#ffad5d] -mt-[161px]">
+      <div className="w-full h-full bg-[url('/img/background-2.png')] bg-cover bg-center bg-no-repeat"></div>
     </div>
   );
 };
