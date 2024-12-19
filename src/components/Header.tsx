ae32207d04735369d21252721c6ea7318bcf1e44
lucky-elsa <beautylotus718@gmail.com>
2024-12-19 15:48:25 +0800

    feat: added download page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,15 @@
 import React from "react";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <div className="fixed top-0 left-0 flex justify-between items-center bg-black w-full h-40 pl-20 pr-28 z-[9999]">
-      <p className="flex-1 press-start text-white text-2xl uppercase">
+      <Link
+        href="/"
+        className="flex-1 press-start text-white text-2xl uppercase"
+      >
         stavalley
-      </p>
+      </Link>
       <div className="flex gap-11 items-center">
         <div className="flex gap-8 items-center">
           <a href="">
